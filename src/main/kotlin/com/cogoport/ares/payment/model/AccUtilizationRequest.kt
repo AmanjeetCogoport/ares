2c3723797a8db545db375cea51e62d1b7b8381fc
moazzam <mohammad.moazzam@cogoport.com>
2022-05-19 12:41:17 +0530

    acc_utilization common model committed.

---
+++ src/main/kotlin/com/cogoport/ares/payment/model/AccUtilizationRequest.kt
@@ -0,0 +1,20 @@
+package com.cogoport.ares.payment.model
+import java.math.BigDecimal
+
+data class AccUtilizationRequest (
+    var documentNo:Long,
+    var entityCode:Int,
+    var entityId:String,
+    var orgSerialId:Long,
+    var sageOrganizationId:String?,
+    var organizationId:String?,
+    var organizationName:String?,
+    var accCode:Int,
+    var accType:String,
+    var accMode:String,
+    var signFlag:Short,
+    var currencyAmount:BigDecimal,
+    var ledgerAmount:BigDecimal,
+    var currencyPayment:BigDecimal,
+    var ledgerPayment:BigDecimal
+)
