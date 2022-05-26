81524fb010eeae24a99ed609906e12169266ce15
prajjwalgupta <prajjwal.gupta@cogoport.com>
2022-05-26 16:14:50 +0530

    validatioins

--- model/src/main/kotlin/com/cogoport/ares/model/payment/Payment.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/Payment.kt
@@ -1,6 +1,7 @@
 package com.cogoport.ares.model.payment
 
 import io.micronaut.core.annotation.Introspected
+import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -9,28 +10,34 @@ import java.util.UUID
 @Introspected
 data class Payment(
     var id: Long?,
+    @field:NotNull(message = "Entity Type is required")
     var entityType: Int,
-    var entityId: UUID,
     var fileId: Long? = null,
-    var orgSerialId: Long,
-    var customerId: UUID,
-    var customerName: String,
+    var orgSerialId: Long?,
     var sageOrganizationId: String?,
+    var customerId: UUID?,
+    var customerName: String?,
+    @field:NotNull(message = "Account Code is required")
     var accCode: Int,
+    @field:NotNull(message = "Account Mode is required")
     var accMode: AccMode,
     var signFlag: Int,
+    @field:NotNull(message = "Currency Type is required")
     var currencyType: String,
+    @field:NotNull(message = "Amount is required")
     var amount: BigDecimal,
+    @field:NotNull(message = "Ledger Currency is required")
     var ledCurrency: String,
+    @field:NotNull(message = "Ledger Amount is required")
     var ledAmount: BigDecimal,
     var payMode: PayMode?,
     var remarks: String? = null,
-    var bankId: Int?,
+    var bankId: Int,
     var utr: String?,
     var refPaymentId: Long?,
     var transactionDate: LocalDate?,
     var isPosted: Boolean,
     var isDeleted: Boolean,
     var createdAt: LocalDateTime?,
-    var updatedAt: LocalDateTime
+    var modifiedAt: LocalDateTime?
 )
