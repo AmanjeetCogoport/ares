3ae5343ae5d59e0427f3081649f17e1bd710c30c
mohit2mogera <mohit.mogera@cogoport.com>
2022-06-02 19:30:12 +0530

    pr comments fixes

--- model/src/main/kotlin/com/cogoport/ares/model/payment/AccountUtilizationEnum.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/AccountUtilizationEnum.kt
@@ -1,5 +1,9 @@
 package com.cogoport.ares.model.payment
 
+enum class AccMode {
+    AR, AP;
+}
+
 enum class ServiceType {
     FCL_FREIGHT, LCL_FREIGHT, AIR_FREIGHT, FTL_FREIGHT, LTL_FREIGHT, HAULAGE_FREIGHT, FCL_CUSTOMS, AIR_CUSTOMS, LCL_CUSTOMS
 }
