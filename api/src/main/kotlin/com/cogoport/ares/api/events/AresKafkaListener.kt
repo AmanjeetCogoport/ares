054d9ad530d95101545feb5fb841905abea8da28
prajjwalgupta <prajjwal.gupta@cogoport.com>
2022-06-03 22:52:59 +0530

    Kafkalistener for Knockoff

--- api/src/main/kotlin/com/cogoport/ares/api/events/AresKafkaListener.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/events/AresKafkaListener.kt
@@ -1,8 +1,10 @@
 package com.cogoport.ares.api.events
 
 import com.cogoport.ares.api.payment.service.interfaces.InvoiceService
+import com.cogoport.ares.api.payment.service.interfaces.KnockoffService
 import com.cogoport.ares.api.payment.service.interfaces.OpenSearchService
 import com.cogoport.ares.model.payment.AccountUtilizationEvent
+import com.cogoport.ares.model.payment.KnockOffUtilizationEvent
 import io.micronaut.configuration.kafka.annotation.KafkaListener
 import io.micronaut.configuration.kafka.annotation.OffsetReset
 import io.micronaut.configuration.kafka.annotation.Topic
@@ -17,6 +19,8 @@ class AresKafkaListener {
     @Inject
     private lateinit var openSearchService: OpenSearchService
 
+    @Inject
+    lateinit var knockoffService: KnockoffService
     @Topic("account-utilization")
     fun listenAccountUtilization(accountUtilizationEvent: AccountUtilizationEvent) = runBlocking {
         invoiceService.addAccountUtilization(accountUtilizationEvent.accUtilizationRequest)
@@ -25,5 +29,10 @@ class AresKafkaListener {
     @Topic("receivables-dashboard-data")
     fun listenDashboardData(openSearchEvent: OpenSearchEvent) = runBlocking {
         openSearchService.pushDashboardData(openSearchEvent.openSearchRequest)
+    }
+
+    @Topic("knockoff-payables")
+    fun knockoffPayables(knockOffUtilizationEvent: KnockOffUtilizationEvent) = runBlocking {
+        knockoffService.uploadBillPayment(knockOffUtilizationEvent.knockOffUtilizationRequest)
     }
 }
