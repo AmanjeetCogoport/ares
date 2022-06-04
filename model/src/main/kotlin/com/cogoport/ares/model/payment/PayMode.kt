cc523c17f56dad0ec19cadfbb8e7d770f3853f35
moazzam <mohammad.moazzam@cogoport.com>
2022-06-04 13:42:14 +0530

    gradle file updated to support transaction payable file updated for org serial id Payment mode value changes

--- model/src/main/kotlin/com/cogoport/ares/model/payment/PayMode.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/PayMode.kt
@@ -1,5 +1,5 @@
 package com.cogoport.ares.model.payment
 
 enum class PayMode(dbValue: String) {
-    DD("DD"), CASH("CASH"), CHEQUE("CHEQUE"), NET_BANKING("NET_BANKING"), UPI("UPI"), BANK("BANK")
+    DD("IMPS"), CASH("CASH"), CHEQUE("CHQ"), NET_BANKING("NEFT"), UPI("RTGS"), BANK("BANK")
 }
