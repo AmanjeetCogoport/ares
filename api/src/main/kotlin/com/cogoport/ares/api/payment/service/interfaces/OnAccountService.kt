363c4cc6a55d190e0d90208e08abe7b55fd53b6a
moazzam <mohammad.moazzam@cogoport.com>
2022-06-06 13:34:39 +0530

    client changes

--- api/src/main/kotlin/com/cogoport/ares/api/payment/service/interfaces/OnAccountService.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/service/interfaces/OnAccountService.kt
@@ -1,13 +1,13 @@
 package com.cogoport.ares.api.payment.service.interfaces
 
+import com.cogoport.ares.model.payment.AccountCollectionRequest
 import com.cogoport.ares.model.payment.AccountCollectionResponse
 import com.cogoport.ares.model.payment.BulkPaymentResponse
 import com.cogoport.ares.model.payment.OnAccountApiCommonResponse
 import com.cogoport.ares.model.payment.Payment
-import java.time.LocalDateTime
 
 interface OnAccountService {
-    suspend fun getOnAccountCollections(uploadedDate: LocalDateTime?, entityType: Int?, currencyType: String?): AccountCollectionResponse
+    suspend fun getOnAccountCollections(request: AccountCollectionRequest): AccountCollectionResponse
     suspend fun createPaymentEntry(receivableRequest: Payment): OnAccountApiCommonResponse
     suspend fun updatePaymentEntry(receivableRequest: Payment): OnAccountApiCommonResponse
     suspend fun deletePaymentEntry(paymentId: Long): OnAccountApiCommonResponse
