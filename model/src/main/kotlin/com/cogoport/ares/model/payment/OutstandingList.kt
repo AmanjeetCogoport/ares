2d83c7e285bd2b9bb3019725f772a8abd07b37e0
technaresh <naresh.choudhary@cogoport.com>
2022-06-09 12:27:51 +0530

    outstanding fixes

--- model/src/main/kotlin/com/cogoport/ares/model/payment/OutstandingList.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/OutstandingList.kt
@@ -1,6 +1,7 @@
 package com.cogoport.ares.model.payment
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.core.annotation.ReflectiveAccess
@@ -8,9 +9,10 @@ import io.micronaut.core.annotation.ReflectiveAccess
 @Introspected
 @ReflectiveAccess
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonInclude
 data class OutstandingList(
     @JsonProperty("list")
-    val list: MutableList<CustomerOutstanding?>,
+    val list: MutableList<CustomerOutstanding?>? = null,
     @JsonProperty("totalPage")
     val totalPage: Int,
     @JsonProperty("totalRecords")
