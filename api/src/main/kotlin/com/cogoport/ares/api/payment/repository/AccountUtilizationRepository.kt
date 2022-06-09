66e4095b41b8cdced478ceb9b6ce82af6b884769
technaresh <naresh.choudhary@cogoport.com>
2022-06-09 19:48:59 +0530

    bug fixes

--- api/src/main/kotlin/com/cogoport/ares/api/payment/repository/AccountUtilizationRepository.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/repository/AccountUtilizationRepository.kt
@@ -162,7 +162,8 @@ interface AccountUtilizationRepository : CoroutineCrudRepository<AccountUtilizat
             abs(sum(case when acc_type = 'REC' then sign_flag*(amount_loc - pay_loc) else 0 end)) as on_account_payment,
             sum(case when acc_type in ('SINV','SDN','SCN') then sign_flag*(amount_loc - pay_loc) else 0 end) + sum(case when acc_type = 'REC' then sign_flag*(amount_loc - pay_loc) else 0 end) as outstandings,
             sum(case when acc_type in ('SINV','SDN','SCN') and transaction_date >= date_trunc('month',(:date)::date) then sign_flag*amount_loc end) as total_sales,
-            date_part('days',(:date)::date) as days
+            case when extract(month from :date::date) < extract(month from now()::date) then date_part('days',date_trunc('month',(:date::date + '1 month'::interval)) - '1 day'::interval) 
+            else date_part('days', :date::date) end as days
             from account_utilizations
             where (:zone is null or zone_code = :zone) and document_status = 'FINAL' and acc_mode = 'AR' and transaction_date <= :date::date
             )
@@ -181,7 +182,8 @@ interface AccountUtilizationRepository : CoroutineCrudRepository<AccountUtilizat
             abs(sum(case when acc_type = 'PAY' then sign_flag*(amount_loc - pay_loc) else 0 end)) as on_account_payment,
             sum(case when acc_type in ('PINV','PDN','PCN') then sign_flag*(amount_loc - pay_loc) else 0 end) + sum(case when acc_type = 'PAY' then sign_flag*(amount_loc - pay_loc) else 0 end) as outstandings,
             sum(case when acc_type in ('PINV','PDN','PCN') and transaction_date >= date_trunc('month',transaction_date) then sign_flag*amount_loc end) as total_sales,
-            date_part('days',(:date)::date) as days
+            case when extract(month from :date::date) < extract(month from now()::date) then date_part('days',date_trunc('month',(:date::date + '1 month'::interval)) - '1 day'::interval) 
+            else date_part('days', :date::date) end as days
             from account_utilizations
             where (:zone is null or zone_code = :zone) and acc_mode = 'AP' and document_status = 'FINAL' and transaction_date <= :date::date
         )
