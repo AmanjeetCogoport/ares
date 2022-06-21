d48a2aafd1c46a41cea9c4a44326266976141a23
technaresh <naresh.choudhary@cogoport.com>
2022-06-21 19:42:16 +0530

    fixes

--- model/src/main/kotlin/com/cogoport/ares/model/payment/OrganizationReceivablesRequest.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/OrganizationReceivablesRequest.kt
@@ -1,8 +1,9 @@
 package com.cogoport.ares.model.payment
 
 import com.cogoport.ares.model.common.AresModelConstants
+import io.micronaut.core.annotation.Introspected
 import io.micronaut.http.annotation.QueryValue
-
+@Introspected
 data class OrganizationReceivablesRequest(
     @QueryValue(AresModelConstants.ORG_ID) val orgId: String? = null,
     @QueryValue(AresModelConstants.YEAR) val year: Int = AresModelConstants.CURR_YEAR,
