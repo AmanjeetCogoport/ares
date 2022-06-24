b6858896cdb39fc7d87d812f2bacce84f2e0a199
moazzam <mohammad.moazzam@cogoport.com>
2022-06-24 21:09:33 +0530

    bank_id

--- api/src/main/kotlin/com/cogoport/ares/api/payment/repository/PaymentRepository.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/repository/PaymentRepository.kt
@@ -16,7 +16,7 @@ interface PaymentRepository : CoroutineCrudRepository<Payment, Long> {
              select id,entity_code,org_serial_id,sage_organization_id,organization_id,organization_name,
              acc_code,acc_mode,sign_flag,currency,amount,led_currency,led_amount,pay_mode,narration,
              trans_ref_number,ref_payment_id,transaction_date::timestamp as transaction_date,is_posted,is_deleted,created_at,updated_at,
-             cogo_account_no,ref_account_no,payment_code,bank_name,payment_num,payment_num_value,exchange_rate,bankId
+             cogo_account_no,ref_account_no,payment_code,bank_name,payment_num,payment_num_value,exchange_rate,bank_id
              from payments where id =:id 
         """
     )
