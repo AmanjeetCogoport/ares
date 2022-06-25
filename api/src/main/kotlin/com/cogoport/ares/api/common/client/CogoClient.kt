84ffec3ebe0d47a24abb35f320869f5073e7e643
moazzam <mohammad.moazzam@cogoport.com>
2022-06-25 12:11:37 +0530

    bug resolved

--- api/src/main/kotlin/com/cogoport/ares/api/common/client/CogoClient.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/common/client/CogoClient.kt
@@ -11,13 +11,14 @@ interface CogoClient {
 
     @Get("/list_cogo_entities?filters%5Bentity_code%5D={entityCode}")
     @Header(name = "authorizationparameters", value = "business_finance-jobs:allowed")
-    @Header(name = "authorization", value = "Bearer: 0cb31ec4-a850-48de-9998-39f93f073f24")
+    @Header(name = "authorization", value = "Bearer: 7cff5415-6007-4bab-9f3e-e4a08c11e915")
     @Header(name = "authorizationscope", value = "partner")
     suspend fun getCogoBank(entityCode: Int): CogoBankResponse
 
     @Get("/get_organization_zone_via_org_id?organization_id={id}")
     @Header(name = "authorizationparameters", value = "business_finance-account_receivables:allowed")
-    @Header(name = "authorization", value = "Bearer: 1d9e275f-7981-44c2-b01f-390bec756606")
+    @Header(name = "authorization", value = "Bearer: 7cff5415-6007-4bab-9f3e-e4a08c11e915")
     @Header(name = "authorizationscope", value = "partner")
     suspend fun getCogoOrganization(id: String): PlatformOrganizationResponse
+
 }
