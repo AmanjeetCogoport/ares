ae966027a06338e6aa1060ebcb7f2ca48b9c7ccf
moazzam <mohammad.moazzam@cogoport.com>
2022-07-01 22:16:00 +0530

    committed

--- model/src/main/kotlin/com/cogoport/ares/model/payment/PlatformOrganizationResponse.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/PlatformOrganizationResponse.kt
@@ -3,10 +3,14 @@ package com.cogoport.ares.model.payment
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
+import java.util.UUID
 
 @Introspected
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class PlatformOrganizationResponse(
+
+    @JsonProperty("id")
+    val organizationId:UUID,
 
     @JsonProperty("organization_name")
     val organizationName: String?,
