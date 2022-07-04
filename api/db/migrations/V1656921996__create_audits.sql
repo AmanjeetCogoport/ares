a5a8ecb039550d11823c290c9a0416d84b114295
technaresh <naresh.choudhary@cogoport.com>
2022-07-04 13:41:50 +0530

    create audit table for account utilization

---
+++ api/db/migrations/V1656921996__create_audits.sql
@@ -0,0 +1,12 @@
+CREATE TABLE account_utilization_audits
+(
+    id          BIGSERIAL PRIMARY KEY,
+    object_type VARCHAR(20) NOT NULL,
+    object_id   BIGINT,
+    action_name VARCHAR(20) NOT NULL,
+    data        JSONB,
+    performed_by  UUID, -- Can be null in case of system triggered actions
+    performed_by_user_type VARCHAR(50),
+    created_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
