6127b9307176300def15768729cae0fb14f4647e
sankalpbhatt <sankalp.bhatt@cogoport.com>
2022-07-06 15:12:36 +0530

    Update migration, enum for settlement type

--- api/db/migrations/V1657026805__ddl_settlements_table.sql
+++ api/db/migrations/V1657026805__ddl_settlements_table.sql
@@ -1,13 +1,28 @@
+CREATE TYPE public."settlement_type" AS ENUM (
+    	'SINV',
+    	'PINV',
+    	'SCN',
+    	'SDN',
+    	'PCN',
+    	'PDN',
+    	'REC',
+    	'PAY',
+    	'SECH',
+    	'PECH',
+    	'CTDS',
+    	'VTDS');
+
 create table public.”settlements”(
     id bigserial constraint pim_PK primary key,
     source_id bigint,
-    source_type ACCOUNT_TYPE,
+    source_type SETTLEMENT_TYPE,
     destination_id bigint,
-    destination_type ACCOUNT_TYPE,
+    destination_type SETTLEMENT_TYPE,
     currency varchar(5),
-    amount decimal(13,4) ,
+    amount decimal(13,4),
     led_currency varchar(5),
     led_amount decimal(13,4),
+    sign_flag smallint,
     settlement_date date,
     created_at timestamp not null default now(),
     updated_at timestamp not null default now()
---
+++ model/src/main/kotlin/com/cogoport/ares/model/settlement/SettlementType.kt
@@ -0,0 +1,16 @@
+package com.cogoport.ares.model.settlement
+
+import com.cogoport.ares.model.payment.AccountType
+
+enum class SettlementType(dbValue: String) {
+    SINV("SINV"), PINV("PINV"), SCN("SCN"), SDN("SDN"), PCN("PCN"), PDN("PDN"), REC("REC"), PAY("PAY"), SECH("SECH"), PECH("PECH"), VTDS("VTDS"), CTDS("CTDS");
+
+    open operator fun contains(value: String?): Boolean {
+        for (c in AccountType.values()) {
+            if (c.equals(value)) {
+                return true
+            }
+        }
+        return false
+    }
+}
