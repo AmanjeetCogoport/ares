537bf57c3d2817c5955ebff9e92521fed1ea072a
susheel-cogoport <susheel.gupta@cogoport.com>
2022-07-07 11:48:56 +0530

    fix of cogo client

---
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/CogoEntitiesRequest.kt
@@ -0,0 +1,9 @@
+package com.cogoport.ares.model.payment
+
+import io.micronaut.core.annotation.Introspected
+
+@Introspected
+data class CogoEntitiesRequest (
+    var authToken: String,
+    var entityCode: Int
+)
