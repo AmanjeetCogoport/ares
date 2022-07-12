2dd5af7968d921dfbf3101e1f33b22cc64708e28
sankalpbhatt <sankalp.bhatt@cogoport.com>
2022-07-12 21:02:51 +0530

    Mandatory fields in input for settlement list documents

--- api/src/main/kotlin/com/cogoport/ares/api/settlement/service/implementation/SettlementServiceImpl.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/settlement/service/implementation/SettlementServiceImpl.kt
@@ -315,6 +315,8 @@ open class SettlementServiceImpl : SettlementService {
         for (doc in documentModel) {
             doc.documentType = getInvoiceType(AccountType.valueOf(doc.documentType))
             doc.status = getInvoiceStatus(doc.afterTdsAmount, doc.balanceAmount)
+            doc.settledAllocation = BigDecimal.ZERO
+            doc.settledTds = BigDecimal.ZERO
         }
         return ResponseList(
             list = documentModel,
@@ -443,7 +445,7 @@ open class SettlementServiceImpl : SettlementService {
             val ledgerRate = payment.exchangeRate
             var updateDoc = true
             if (payment.currency != invoice.currency) {
-                rate = if (payment.legCurrency == invoice.currency) {
+                rate = if (payment.ledCurrency == invoice.currency) {
                     ledgerRate
                 } else {
                     getExchangeRate(payment.currency, invoice.currency, payment.transactionDate)
@@ -485,17 +487,17 @@ open class SettlementServiceImpl : SettlementService {
         val invoiceTds = invoice.tds!! - invoice.settledTds
         val paymentTds = getExchangeValue(invoiceTds, exchangeRate, true)
         val paymentTdsLed = getExchangeValue(paymentTds, ledgerRate)
-        val doc = createSettlement(payment.documentNo, payment.accountType, invoice, payment.currency, (paidAmount + paymentTds), payment.legCurrency, (paidLedAmount + paymentTdsLed), 1, request.settlementDate)
+        val doc = createSettlement(payment.documentNo, payment.accountType, invoice, payment.currency, (paidAmount + paymentTds), payment.ledCurrency, (paidLedAmount + paymentTdsLed), 1, request.settlementDate)
         if (paymentTds.compareTo(0.toBigDecimal()) != 0) {
             val tdsType = if (fetchSettlingDocs(SettlementType.CTDS).contains(invoice.accountType)) SettlementType.CTDS else SettlementType.VTDS
-            val tdsDoc = createSettlement(payment.documentNo, tdsType, invoice, payment.currency, paymentTds, invoice.legCurrency, paymentTdsLed, -1, request.settlementDate)
+            val tdsDoc = createSettlement(payment.documentNo, tdsType, invoice, payment.currency, paymentTds, invoice.ledCurrency, paymentTdsLed, -1, request.settlementDate)
             invoice.settledTds += invoiceTds
         }
-        if (payment.legCurrency != invoice.currency) {
+        if (payment.ledCurrency != invoice.currency) {
             val excLedAmount = getExchangeValue(toSettleAmount, invoice.exchangeRate) - (paidLedAmount)
             val exType = if (fetchSettlingDocs(SettlementType.CTDS).contains(invoice.accountType)) SettlementType.SECH else SettlementType.PECH
             val exSign = excLedAmount.signum() * if (payment.accountType in listOf(SettlementType.SCN, SettlementType.REC, SettlementType.SINV)) -1 else 1
-            val excDoc = createSettlement(payment.documentNo, exType, invoice, null, null, invoice.legCurrency, excLedAmount.abs(), exSign.toShort(), request.settlementDate)
+            val excDoc = createSettlement(payment.documentNo, exType, invoice, null, null, invoice.ledCurrency, excLedAmount.abs(), exSign.toShort(), request.settlementDate)
         }
         val paymentUtilized = paidAmount + if (payment.accountType in listOf(SettlementType.PCN, SettlementType.SCN)) paymentTds else 0.toBigDecimal()
         updateAccountUtilization(payment, paymentUtilized)
