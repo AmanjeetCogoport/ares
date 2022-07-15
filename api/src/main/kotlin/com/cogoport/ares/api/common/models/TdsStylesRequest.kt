6b16412168b9060334f2f8efa419de65881da31f
technaresh <naresh.choudhary@cogoport.com>
2022-07-15 16:20:24 +0530

    redundant

--- api/src/main/kotlin/com/cogoport/ares/api/common/models/TdsStylesRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.cogoport.ares.api.common.models
-
-import io.micronaut.core.annotation.Introspected
-import io.micronaut.http.annotation.QueryValue
-
-@Introspected
-data class TdsStylesRequest(
-    @QueryValue("id") var id: String
-)
