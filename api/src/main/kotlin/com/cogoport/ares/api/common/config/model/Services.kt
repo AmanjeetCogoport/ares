042579b627c74eafe66c4026691f623e6e97e1be
sankalpbhatt <sankalp.bhatt@cogoport.com>
2022-07-15 19:21:51 +0530

    Revert "Merge pull request #235 from Cogoport/f-settlement-invoice-sid"
    This reverts commit db0b821a286de81271397be913e1208af5b078b8, reversing
    changes made to 489285102f73cdeb69c59be82480e71df449d7e2.

--- api/src/main/kotlin/com/cogoport/ares/api/common/config/model/Services.kt
+++
@@ -1,8 +0,0 @@
-package com.cogoport.ares.api.common.config.model
-
-import io.micronaut.context.annotation.ConfigurationProperties
-
-@ConfigurationProperties("services")
-class Services {
-    public var service: String? = null
-}
