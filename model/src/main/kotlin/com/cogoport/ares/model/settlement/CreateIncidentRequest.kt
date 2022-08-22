a429ac57d3ddc89712342623a53488c3c865c1af
technaresh <naresh.choudhary@cogoport.com>
2022-08-22 21:55:37 +0530

    updated hades model and client

--- model/src/main/kotlin/com/cogoport/ares/model/settlement/CreateIncidentRequest.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/settlement/CreateIncidentRequest.kt
@@ -7,8 +7,8 @@ import javax.validation.constraints.NotNull
 
 @Introspected
 data class CreateIncidentRequest(
-    @field:NotNull(message = "orgId is required") var orgId: UUID?,
-    @field:NotNull(message = "orgName is required") var orgName: String?,
+    var orgId: UUID?,
+    var orgName: String?,
     @field:NotNull(message = "stackDetails is required") var stackDetails: List<CheckDocument>?,
     @field:NotNull(message = "createdBy is required") var createdBy: UUID?,
     @field:NotNull(message = "entityCode is required") var entityCode: Int?,
