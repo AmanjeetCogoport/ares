385369cb67e133f34c6726ad45966bc504212f38
Arpitrajoria <arpit.rajoria@cogoport.com>
2022-10-14 16:56:14 +0530

    changess

--- client/src/main/kotlin/com/cogoport/ares/client/AresClient.kt
+++ client/src/main/kotlin/com/cogoport/ares/client/AresClient.kt
@@ -136,5 +136,5 @@ interface AresClient {
     suspend fun reachable(): HttpResponse<String>
 
     @Get("/payments/invoice/get-curr-outstanding-on-day-of-computation")
-    suspend fun getCurrOutstanding(@QueryValue req:List<Long>)
+    suspend fun getCurrOutstanding(@QueryValue req: List<Long>)
 }
