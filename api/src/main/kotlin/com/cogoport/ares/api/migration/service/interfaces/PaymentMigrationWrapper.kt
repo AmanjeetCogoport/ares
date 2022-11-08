3ec34045ac436ddbd85c8933ade67c38c7627193
pramod1312 <pramod.sharma@cogoport.com>
2022-11-08 21:03:33 +0530

    payment migration code changes

--- api/src/main/kotlin/com/cogoport/ares/api/migration/service/interfaces/PaymentMigrationWrapper.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/migration/service/interfaces/PaymentMigrationWrapper.kt
@@ -3,4 +3,5 @@ package com.cogoport.ares.api.migration.service.interfaces
 interface PaymentMigrationWrapper {
     suspend fun migratePaymentsFromSage(startDate: String?, endDate: String?): Int
     suspend fun migrateJournalVoucher(startDate: String?, endDate: String?): Int
+    suspend fun migratePaymentsByBpr(bpr: String, mode: String): Int
 }
