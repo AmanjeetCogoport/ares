d03c8ad0319d8424e28458e07874632ec6e18c58
Garima Singh <garima.singh@cogoport.com>
2022-11-10 11:43:31 +0530

    remove UNIQUE constraint from business name

--- api/db/migrations/V1668060652__create_table_defaulted_business_partners.sql
+++ api/db/migrations/V1668060652__create_table_defaulted_business_partners.sql
@@ -1,7 +1,7 @@
 CREATE TABLE defaulted_business_partners
 (
     id  BIGSERIAL PRIMARY KEY,
-    business_name    VARCHAR(100) NOT NULL UNIQUE,
+    business_name    VARCHAR(100) NOT NULL,
     trade_party_detail_serial_id  BIGSERIAL NOT NULL UNIQUE,
     sage_org_id   VARCHAR(20) NOT NULL UNIQUE,
     trade_party_detail_id  UUID NOT NULL UNIQUE,
