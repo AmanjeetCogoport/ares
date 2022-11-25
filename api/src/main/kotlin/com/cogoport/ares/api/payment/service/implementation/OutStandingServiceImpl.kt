0495f7a4545907ada0dbb80843525d1c8919bf1d
janshikaa <Anshika.Jain@cogoport.com>
2022-11-25 09:51:07 +0530

    ktlintFormat fixes

--- api/src/main/kotlin/com/cogoport/ares/api/payment/service/implementation/OutStandingServiceImpl.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/service/implementation/OutStandingServiceImpl.kt
@@ -142,8 +142,7 @@ class OutStandingServiceImpl : OutStandingService {
         orgIds.forEach {
             val customerOutstanding = OpenSearchClient().listCustomerOutstandingOfAllZone(index = AresConstants.SALES_OUTSTANDING_INDEX, classType = CustomerOutstanding::class.java, values = "${it}_ALL")
             customerOutstanding?.hits()?.hits()?.map {
-                it.source()?.let {
-                    it1 ->
+                it.source()?.let { it1 ->
                     listOrganization.add(it1)
                 }
             }
