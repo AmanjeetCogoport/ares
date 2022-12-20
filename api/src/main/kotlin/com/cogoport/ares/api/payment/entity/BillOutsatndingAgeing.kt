5ee81d12cd9446445fc8ea762b8182ccece4d7fe
Shivam Shah <110809603+nulllvoid@users.noreply.github.com>
2022-12-20 19:54:46 +0530

    sign changes

--- api/src/main/kotlin/com/cogoport/ares/api/payment/entity/BillOutsatndingAgeing.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/entity/BillOutsatndingAgeing.kt
@@ -1,7 +1,9 @@
 package com.cogoport.ares.api.payment.entity
 
+import io.micronaut.data.annotation.MappedEntity
 import java.math.BigDecimal
 
+@MappedEntity
 data class BillOutsatndingAgeing(
     val organizationId: String?,
     val organizationName: String?,
