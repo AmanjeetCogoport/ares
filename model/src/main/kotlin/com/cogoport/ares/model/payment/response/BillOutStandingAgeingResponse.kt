6142a8749b8a7633f3c4ec1279df6b534342fa50
Shivam Shah <110809603+nulllvoid@users.noreply.github.com>
2022-12-26 20:31:31 +0530

    annotation fix

--- model/src/main/kotlin/com/cogoport/ares/model/payment/response/BillOutStandingAgeingResponse.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/response/BillOutStandingAgeingResponse.kt
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
-import io.micronaut.core.annotation.ReflectiveAccess
 import java.math.BigDecimal
 
 @Introspected
-@ReflectiveAccess
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class BillOutStandingAgeingResponse(
