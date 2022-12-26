74a7e7b4a32b43cfcb3db13696440e72302510ca
Shivam Shah <110809603+nulllvoid@users.noreply.github.com>
2022-12-26 20:23:48 +0530

    annotation fixes

--- model/src/main/kotlin/com/cogoport/ares/model/payment/BillOutstandingList.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/payment/BillOutstandingList.kt
@@ -5,10 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
-import io.micronaut.core.annotation.ReflectiveAccess
 
 @Introspected
-@ReflectiveAccess
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonInclude
 @JsonIgnoreProperties(ignoreUnknown = true)
