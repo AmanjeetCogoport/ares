9127e614174aa1963ea0269809ad1054b380c7af
sarthak-cogoport <sarthak.gupta@cogoport.com>
2023-01-12 21:40:41 +0530

    ktlint

--- api/src/main/kotlin/com/cogoport/ares/api/payment/service/implementation/OutStandingServiceImpl.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/service/implementation/OutStandingServiceImpl.kt
@@ -279,7 +279,6 @@ class OutStandingServiceImpl : OutStandingService {
             logger().error(error.toString())
             logger().error(error.stackTraceToString())
         }
-
     }
 
     /**
