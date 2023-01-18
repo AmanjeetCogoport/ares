88f8a24d13053671d79b4429e2664d33aca454f0
sarthak-cogoport <sarthak.gupta@cogoport.com>
2023-01-18 11:40:01 +0530

    changes

--- api/src/main/kotlin/com/cogoport/ares/api/events/AresKafkaListener.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/events/AresKafkaListener.kt
@@ -24,9 +24,12 @@ import kotlinx.coroutines.runBlocking
 import org.apache.kafka.clients.consumer.ConsumerConfig
 
 @KafkaListener(
-    offsetReset = OffsetReset.LATEST, pollTimeout = "25000ms",
+    offsetReset = OffsetReset.LATEST, pollTimeout = "10000ms",
     offsetStrategy = OffsetStrategy.SYNC_PER_RECORD, threads = 2, heartbeatInterval = "1000ms",
-    properties = [Property(name = ConsumerConfig.MAX_POLL_RECORDS_CONFIG, value = "3")]
+    properties = [
+        Property(name = ConsumerConfig.MAX_POLL_RECORDS_CONFIG, value = "10"),
+        Property(name = ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, value = "20000ms")
+    ]
 )
 class AresKafkaListener {
     @Inject
