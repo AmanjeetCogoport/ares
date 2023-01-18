62dd27111396e7c511e6d1e1899cfd8bf526c83f
sarthak-cogoport <sarthak.gupta@cogoport.com>
2023-01-18 17:56:48 +0530

    changes

---
+++ api/src/main/kotlin/com/cogoport/ares/api/events/KuberMessagePublisher.kt
@@ -0,0 +1,12 @@
+package com.cogoport.ares.api.events
+import com.cogoport.ares.model.payment.RestoreUtrResponse
+import io.micronaut.rabbitmq.annotation.Binding
+import io.micronaut.rabbitmq.annotation.RabbitClient
+import io.micronaut.rabbitmq.annotation.RabbitProperty
+
+@RabbitClient("kuber")
+@RabbitProperty(name = "deliveryMode", value = "2")
+interface KuberMessagePublisher {
+    @Binding("restore.utr")
+    fun emitPostRestoreUtr(restoreUtrResponse: RestoreUtrResponse)
+}
