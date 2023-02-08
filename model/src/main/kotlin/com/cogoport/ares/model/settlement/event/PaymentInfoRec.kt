064f89838c68fc170eb3b8657e467ddb12f3e886
vivekgarg7900 <vivek.garg@cogoport.com>
2023-02-08 16:28:47 +0530

    ktlint

--- model/src/main/kotlin/com/cogoport/ares/model/settlement/event/PaymentInfoRec.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/settlement/event/PaymentInfoRec.kt
@@ -1,11 +1,8 @@
 package com.cogoport.ares.model.settlement.event
 
 import com.cogoport.ares.model.settlement.SettlementType
-import com.fasterxml.jackson.annotation.JsonFormat
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.data.annotation.MappedEntity
-import java.sql.Date
-import java.sql.Timestamp
 
 @Introspected
 @MappedEntity
