cba3f1adc488277ef0f60d986147757636fea5ab
eeshan-1 <eeshan.narula@cogoport.com>
2023-03-21 21:12:07 +0530

    f

--- api/src/main/kotlin/com/cogoport/ares/api/settlement/repository/JournalVoucherRepository.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/settlement/repository/JournalVoucherRepository.kt
@@ -37,7 +37,8 @@ interface JournalVoucherRepository : CoroutineCrudRepository<JournalVoucher, Lon
             j.updated_by,
             j.description as description,
             j.acc_mode,
-            j.parent_jv_id
+            j.parent_jv_id,
+            j.sage_unique_id
             FROM journal_vouchers j
             where 
                 (:status is null OR  status = :status::JV_STATUS) AND
@@ -134,7 +135,8 @@ interface JournalVoucherRepository : CoroutineCrudRepository<JournalVoucher, Lon
             j.updated_by,
             j.description as description,
             j.acc_mode,
-            j.parent_jv_id
+            j.parent_jv_id,
+            j.sage_unique_id
             FROM journal_vouchers j 
             Where 
                 j.parent_jv_id = :parentId
