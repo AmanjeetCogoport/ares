03cfa7b10cf0d7ab0816feec7b5d4867a95f1bc0
pramod1312 <pramod.sharma@cogoport.com>
2023-03-27 18:12:04 +0530

    consider all types jv

--- api/src/main/kotlin/com/cogoport/ares/api/migration/service/implementation/SageServiceImpl.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/migration/service/implementation/SageServiceImpl.kt
@@ -116,11 +116,11 @@ class SageServiceImpl : SageService {
             (
             select TYP_0,NUM_0,FCY_0,CUR_0,SAC_0,BPR_0,DUDDAT_0,PAM_0,SUM(AMTCUR_0) as AMTCUR_0,SUM(AMTLOC_0) as AMTLOC_0,SUM(PAYCUR_0) as PAYCUR_0,SUM(PAYLOC_0) as PAYLOC_0
             ,MAX(SNS_0) as sign_flag
-            from  COGO2.GACCDUDATE where SAC_0 in('AR','SC') and TYP_0 in ('BANK','CONTR','INTER','MTC','MTCCV','OPDIV','MISC') 
+            from  COGO2.GACCDUDATE where SAC_0 in('AR','SC') -- and TYP_0 in ('BANK','CONTR','INTER','MTC','MTCCV','OPDIV','MISC') 
             GROUP BY TYP_0,NUM_0,FCY_0,CUR_0,SAC_0,BPR_0,DUDDAT_0,PAM_0
             ) G 
             on (GC.NUM_0 = G.NUM_0 and GC.FCY_0=G.FCY_0)
-            where G.SAC_0 in('AR','SC') and G.TYP_0 in ('BANK','CONTR','INTER','MTC','MTCCV','OPDIV','MISC')
+            where G.SAC_0 in('AR','SC') -- and G.TYP_0 in ('BANK','CONTR','INTER','MTC','MTCCV','OPDIV','MISC')
             and G.BPR_0 not in ${MigrationConstants.administrativeExpense}
             """
         if (startDate == null && endDate == null) {
