81d6cebcca09b192dbd6e160691571065f5ce8e1
keshavsingh <keshav.singh@cogoport.com>
2023-03-28 16:12:46 +0530

    jv_category to varchar

--- api/db/migrations/V1679999506__altering_jv_category_from_enum_varchar.sql
+++ api/db/migrations/V1679999506__altering_jv_category_from_enum_varchar.sql
@@ -1,3 +1,5 @@
-UPDATE journal_vouchers SET category_varchar = category::varchar;
+ALTER TABLE journal_vouchers ADD COLUMN category_varchar VARCHAR;
+
+UPDATE journal_vouchers SET category_varchar = category::text;
 ALTER TABLE journal_vouchers DROP COLUMN category;
 ALTER TABLE journal_vouchers RENAME COLUMN category_varchar TO category;
