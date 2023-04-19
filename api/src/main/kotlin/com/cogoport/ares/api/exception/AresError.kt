e5e7a604fc0acfa3826fac559e733f072ba91ac7
vivekgarg7900 <vivek.garg@cogoport.com>
2023-04-19 17:46:08 +0530

    post-to-sage changes

--- api/src/main/kotlin/com/cogoport/ares/api/exception/AresError.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/exception/AresError.kt
@@ -53,7 +53,8 @@ enum class AresError(
     ERR_1526("ERR_1526", "Unauthorised", HttpStatus.UNAUTHORIZED),
     ERR_1527("ERR_1527", "Credit Amount is not equal to Debit Amount", HttpStatus.NOT_ACCEPTABLE),
     ERR_1528("ERR_1528", "Jv is yet not been approved.", HttpStatus.NOT_ACCEPTABLE),
-    ERR_1529("ERR_1529", "No value exists for this Acc Mode: ", HttpStatus.BAD_REQUEST);
+    ERR_1529("ERR_1529", "No value exists for this Acc Mode: ", HttpStatus.BAD_REQUEST),
+    ERR_1530("ERR_1530", "No Organization is present with this Trade Party Id ", HttpStatus.BAD_REQUEST);
 
     fun getMessage(param: String): String {
         if (param.isNotEmpty()) {
