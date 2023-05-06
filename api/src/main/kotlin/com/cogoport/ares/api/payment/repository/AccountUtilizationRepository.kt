4d7e516e5c3b71d02ab2828c7fa8d4ea52faeccd
keshavsingh <keshav.singh@cogoport.com>
2023-05-06 10:41:22 +0530

    changes

--- api/src/main/kotlin/com/cogoport/ares/api/payment/repository/AccountUtilizationRepository.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/repository/AccountUtilizationRepository.kt
@@ -52,7 +52,7 @@ interface AccountUtilizationRepository : CoroutineCrudRepository<AccountUtilizat
     @Query(
         """select id,document_no,document_value , zone_code,service_type,document_status,entity_code , category,org_serial_id,sage_organization_id
            ,organization_id, tagged_organization_id, trade_party_mapping_id, organization_name,acc_code,acc_type,acc_mode,sign_flag,currency,led_currency,amount_curr, amount_loc,pay_curr
-           ,pay_loc,due_date,transaction_date,created_at,updated_at, taxable_amount, migrated, is_void,tagged_bill_id, tds_amount, tds_amount_loc, is_settlement
+           ,pay_loc,due_date,transaction_date,created_at,updated_at, taxable_amount, migrated, is_void,tagged_bill_id, tds_amount, tds_amount_loc, settlement_enabled
             from account_utilizations where document_no = :documentNo and (:accType is null or acc_type= :accType::account_type) 
             and (:accMode is null or acc_mode=:accMode::account_mode) and deleted_at is null and is_void = false"""
     )
@@ -62,7 +62,7 @@ interface AccountUtilizationRepository : CoroutineCrudRepository<AccountUtilizat
     @Query(
         """select id,document_no,document_value , zone_code,service_type,document_status,entity_code , category,org_serial_id,sage_organization_id
            ,organization_id, tagged_organization_id, trade_party_mapping_id,organization_name,acc_code,acc_type,acc_mode,sign_flag,currency,led_currency,amount_curr, amount_loc,pay_curr
-           ,pay_loc,due_date,transaction_date,created_at,updated_at, taxable_amount, migrated,tagged_bill_id, is_void, tds_amount, tds_amount_loc, is_settlement
+           ,pay_loc,due_date,transaction_date,created_at,updated_at, taxable_amount, migrated,tagged_bill_id, is_void, tds_amount, tds_amount_loc, settlement_enabled
             from account_utilizations where document_value = :documentValue and (:accType is null or acc_type= :accType::account_type)
             and (:accMode is null or acc_mode=:accMode::account_mode) and deleted_at is null """
     )
@@ -932,7 +932,7 @@ interface AccountUtilizationRepository : CoroutineCrudRepository<AccountUtilizat
                 migrated,
                 tds_amount,
                 tds_amount_loc,
-                is_settlement
+                settlement_enabled
                 FROM account_utilizations
                 WHERE document_value = :documentValue
                 AND acc_type = :accType::account_type
@@ -979,7 +979,7 @@ interface AccountUtilizationRepository : CoroutineCrudRepository<AccountUtilizat
                 tds_amount,
                 tds_amount_loc,
                 migrated,
-                is_settlement
+                settlement_enabled
                 FROM account_utilizations
                 WHERE document_no = :documentNo
                 AND   acc_type = :accType::account_type
@@ -1148,7 +1148,7 @@ interface AccountUtilizationRepository : CoroutineCrudRepository<AccountUtilizat
            au.organization_id,au.tagged_organization_id,au.trade_party_mapping_id, au.organization_name,
            au.acc_code,au.acc_type,au.acc_mode,au.sign_flag,au.currency,au.led_currency,au.amount_curr,
            au.amount_loc,au.pay_curr,au.pay_loc,au.due_date,au.transaction_date,au.updated_at, au.taxable_amount,
-           au.migrated,au.created_at, au.is_void, au.tagged_bill_id, au.tds_amount, au.tds_amount_loc, au.is_settlement
+           au.migrated,au.created_at, au.is_void, au.tagged_bill_id, au.tds_amount, au.tds_amount_loc, au.settlement_enabled
            FROM 
            payments p
            JOIN payment_invoice_mapping pim ON 
