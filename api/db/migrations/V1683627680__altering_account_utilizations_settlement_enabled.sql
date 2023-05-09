f9892dc8ad37abaf82da0e6dffe1529124b0dc59
keshavsingh <keshav.singh@cogoport.com>
2023-05-09 15:57:20 +0530

    update query for migration

--- api/db/migrations/V1683627680__altering_account_utilizations_settlement_enabled.sql
+++ api/db/migrations/V1683627680__altering_account_utilizations_settlement_enabled.sql
@@ -1,2 +1,5 @@
 ALTER TABLE account_utilizations
 ADD COLUMN settlement_enabled bool NOT NULL DEFAULT false;
+
+UPDATE account_utilizations SET
+settlement_enabled = true WHERE document_status = 'FINAL' AND deleted_at is null
