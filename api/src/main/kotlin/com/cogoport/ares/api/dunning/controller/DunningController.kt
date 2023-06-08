9853bc32c09b165cb54c69cb8b2efe80b4f22df9
theyash01 <yash.singh@cogoport.com>
2023-06-08 15:51:03 +0530

    changes

--- api/src/main/kotlin/com/cogoport/ares/api/dunning/controller/DunningController.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/dunning/controller/DunningController.kt
@@ -1,6 +1,21 @@
 package com.cogoport.ares.api.dunning.controller
 
+import com.cogoport.ares.api.dunning.service.interfaces.DunningService
+import com.cogoport.ares.model.payment.request.AccUtilizationRequest
+import com.cogoport.ares.model.payment.response.CreateInvoiceResponse
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import jakarta.inject.Inject
+import javax.validation.Valid
 
 @Controller("/dunning")
-class DunningController
+class DunningController (
+    private val dunningService: DunningService
+        ){
+
+    @Get("/master-list{?request*}")
+    suspend fun listMasterException(@Valid  request: ): List<CreateInvoiceResponse> {
+        return dunningService.listMasterException(request)
+    }
+}
