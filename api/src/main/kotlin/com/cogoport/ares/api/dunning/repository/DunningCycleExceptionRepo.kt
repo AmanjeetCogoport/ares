4e8187d36edd8865b18573c90a7b054d22c0c211
theyash01 <yash.singh@cogoport.com>
2023-06-14 18:07:32 +0530

    changes

--- api/src/main/kotlin/com/cogoport/ares/api/dunning/repository/DunningCycleExceptionRepo.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/dunning/repository/DunningCycleExceptionRepo.kt
@@ -1,9 +1,10 @@
 package com.cogoport.ares.api.dunning.repository
 
 import com.cogoport.ares.api.dunning.entity.DunningCycleExecution
+import io.micronaut.data.annotation.Query
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository
 import io.micronaut.data.repository.kotlin.CoroutineCrudRepository
 
 @R2dbcRepository(dialect = Dialect.POSTGRES)
-interface DunningCycleExecutionRepo : CoroutineCrudRepository<DunningCycleExecution, Long>
+interface DunningCycleExceptionRepo : CoroutineCrudRepository<DunningCycleExecution, Long>
