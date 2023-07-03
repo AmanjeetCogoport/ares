0f14a08f0a277d2f274a87e3ce503aabb7077502
Amanjeet Kumar <amanjeet.cogoport.com>
2023-07-03 14:05:02 +0530

    done

--- api/src/main/kotlin/com/cogoport/ares/api/dunning/service/implementation/DunningServiceImpl.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/dunning/service/implementation/DunningServiceImpl.kt
@@ -193,6 +193,16 @@ open class DunningServiceImpl(
             throw AresException(AresError.ERR_1003, "")
         }
 
+        if (FREQUENCY.valueOf(createDunningCycleRequest.frequency) == FREQUENCY.ONE_TIME) {
+            createDunningCycleRequest.scheduleRule.oneTimeDate = Timestamp(
+                createDunningCycleRequest.scheduleRule.oneTimeDate!!.time.minus(
+                    AresConstants.TIME_ZONE_DIFFENRENCE_FROM_GMT.get(
+                        AresConstants.TimeZone.valueOf(createDunningCycleRequest.scheduleRule.scheduleTimeZone)
+                    ) ?: throw AresException(AresError.ERR_1002, "")
+                )
+            )
+        }
+
         val dunningCycleResponse = dunningCycleRepo.save(
             DunningCycle(
                 id = null,
@@ -1016,11 +1026,13 @@ open class DunningServiceImpl(
         scheduleDateCal.set(Calendar.HOUR_OF_DAY, scheduleHour.toInt())
         scheduleDateCal.set(Calendar.MINUTE, scheduleMinute.toInt())
 
-        val localTimestampWRTZone = System.currentTimeMillis().minus(
-            AresConstants.TIME_ZONE_DIFFENRENCE_FROM_GMT.get(
-                AresConstants.TimeZone.valueOf(scheduleRule.scheduleTimeZone)
-            ) ?: throw AresException(AresError.ERR_1002, "")
-        )?.plus(AresConstants.EXTRA_TIME_TO_PROCESS_DATA_DUNNING)
+//        val localTimestampWRTZone = System.currentTimeMillis().minus(
+//            AresConstants.TIME_ZONE_DIFFENRENCE_FROM_GMT.get(
+//                AresConstants.TimeZone.valueOf(scheduleRule.scheduleTimeZone)
+//            ) ?: throw AresException(AresError.ERR_1002, "")
+//        )?.plus(AresConstants.EXTRA_TIME_TO_PROCESS_DATA_DUNNING)
+
+        val localTimestampWRTZone = System.currentTimeMillis()?.plus(AresConstants.EXTRA_TIME_TO_PROCESS_DATA_DUNNING)
 
         if (scheduleDateCal.timeInMillis < localTimestampWRTZone) {
             throw AresException(AresError.ERR_1551, "")
