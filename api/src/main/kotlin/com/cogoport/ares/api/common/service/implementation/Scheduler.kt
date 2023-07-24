c4c453eb9da847c9d8f422486adc18fa6eaf7b67
vivekgarg7900 <vivek.garg@cogoport.com>
2023-07-24 15:36:46 +0530

    adding pr changes

--- api/src/main/kotlin/com/cogoport/ares/api/common/service/implementation/Scheduler.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/common/service/implementation/Scheduler.kt
@@ -207,7 +207,7 @@ class Scheduler(
         logger().info("Request for mtccv jv migration received, total number of parent jv to migrate is $size")
     }
 
-    @Scheduled(cron = "0 */6 * * *")
+    @Scheduled(cron = "0 17 * * *")
     suspend fun createLedgerSummaryForAp() = runBlocking {
         outStandingService.createLedgerSummary()
     }
