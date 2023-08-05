1bede592e57c4b397d367fd71153bdd198def433
vivekgarg7900 <vivek.garg@cogoport.com>
2023-08-05 12:24:17 +0530

    adidng changes

--- api/src/main/kotlin/com/cogoport/ares/api/payment/entity/LedgerSummary.kt
+++ api/src/main/kotlin/com/cogoport/ares/api/payment/entity/LedgerSummary.kt
@@ -108,10 +108,18 @@ data class LedgerSummary(
     val totalOpenInvoiceAmount: BigDecimal,
     @JsonProperty("total_credit_note_amount")
     val totalCreditNoteAmount: BigDecimal,
-    @JsonProperty("total_on_account_amount")
-    val totalOnAccountAmount: BigDecimal,
+    @JsonProperty("total_open_account_amount")
+    val totalOpenAccountAmount: BigDecimal,
     @JsonProperty("total_outstanding")
     val totalOutstanding: BigDecimal,
+    @JsonProperty("total_invoice_amount")
+    val totalInvoiceAmount: BigDecimal,
+    @JsonProperty("total_on_account_amount")
+    val totalOnAccountAmount: BigDecimal,
+    @JsonProperty("total_on_account_count")
+    val totalOnAccountCount: Long,
+    @JsonProperty("total_invoices_count")
+    val totalInvoicesCount: Long,
     @JsonProperty("created_at")
     val createdAt: Date?
 )
