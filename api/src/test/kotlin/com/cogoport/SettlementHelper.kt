65385ea972dd92903f79a8825684a92f5e7f14d0
vivekgarg7900 <vivek.garg@cogoport.com>
2023-08-07 14:21:26 +0530

    adding

--- api/src/test/kotlin/com/cogoport/SettlementHelper.kt
+++ api/src/test/kotlin/com/cogoport/SettlementHelper.kt
@@ -1,46 +1,43 @@
 package com.cogoport
 
+import com.cogoport.ares.api.common.AresConstants
 import com.cogoport.ares.api.payment.entity.AccountUtilization
+import com.cogoport.ares.api.payment.entity.Payment
 import com.cogoport.ares.api.payment.repository.AccountUtilizationRepo
+import com.cogoport.ares.api.payment.repository.PaymentRepository
+import com.cogoport.ares.api.settlement.entity.JVAdditionalDetails
+import com.cogoport.ares.api.settlement.entity.JournalVoucher
+import com.cogoport.ares.api.settlement.entity.ParentJournalVoucher
+import com.cogoport.ares.api.settlement.entity.Settlement
+import com.cogoport.ares.api.settlement.repository.JournalVoucherRepository
+import com.cogoport.ares.api.settlement.repository.ParentJVRepository
+import com.cogoport.ares.api.settlement.repository.SettlementRepository
 import com.cogoport.ares.model.payment.AccMode
 import com.cogoport.ares.model.payment.AccountType
 import com.cogoport.ares.model.payment.DocumentStatus
+import com.cogoport.ares.model.payment.PayMode
+import com.cogoport.ares.model.payment.PaymentCode
+import com.cogoport.ares.model.payment.PaymentDocumentStatus
 import com.cogoport.ares.model.payment.ServiceType
+import com.cogoport.ares.model.settlement.SettlementType
+import com.cogoport.ares.model.settlement.enums.JVStatus
+import com.cogoport.ares.model.settlement.enums.SettlementStatus
 import jakarta.inject.Singleton
 import java.math.BigDecimal
+import java.sql.Timestamp
+import java.time.Instant
 import java.util.Date
 import java.util.UUID
 
 @Singleton
-class SettlementHelper (
+class SettlementHelper(
     private var accountUtilizationRepo: AccountUtilizationRepo,
+    private var settlementRepository: SettlementRepository,
+    private var paymentRepo: PaymentRepository,
+    private var parentJvRepo: ParentJVRepository,
+    private var journalVoucherRepository: JournalVoucherRepository
 ) {
-    suspend fun saveAccountUtilizations(): Long? {
-        var accUtilDoc = getAccUtilDoc(
-            AccMode.AP,
-            AccountType.PINV,
-            321000,
-            "VIVEK/12234",
-            12345,
-            -1,
-            DocumentStatus.FINAL,
-            301,
-            BigDecimal(40),
-            BigDecimal(40),
-            "INR",
-            "INR",
-            BigDecimal(20),
-            BigDecimal(20),
-            BigDecimal(100),
-            BigDecimal(100),
-        )
-
-        accUtilDoc = accountUtilizationRepo.save(accUtilDoc)
-
-        return accUtilDoc.id
-    }
-
-    private fun getAccUtilDoc(
+    suspend fun saveAccountUtilizations(
         accMode: AccMode,
         accType: AccountType,
         accCode: Int,
@@ -57,12 +54,12 @@ class SettlementHelper (
         tdsAmountLoc: BigDecimal?,
         amountCurr: BigDecimal,
         amountLoc: BigDecimal,
-    ): AccountUtilization {
-        return AccountUtilization(
+    ): Long? {
+        var accUtilDoc = AccountUtilization(
             id = null,
             documentNo = documentNo,
             documentValue = documentValue,
-            accMode =  accMode,
+            accMode = accMode,
             accCode = accCode,
             accType = accType,
             signFlag = signFlag,
@@ -71,7 +68,7 @@ class SettlementHelper (
             category = "ASSET",
             documentStatus = documentStatus,
             dueDate = Date(),
-            entityCode =entityCode,
+            entityCode = entityCode,
             transactionDate = Date(),
             migrated = false,
             organizationId = UUID.fromString("9f03db0c-88cc-450f-bbb1-38fa31861911"),
@@ -88,6 +85,248 @@ class SettlementHelper (
             ledCurrency = ledCurrency,
             tdsAmount = tdsAmount,
             tdsAmountLoc = tdsAmountLoc
+        )
+
+        accUtilDoc = accountUtilizationRepo.save(accUtilDoc)
+
+        return accUtilDoc.id
+    }
+
+    suspend fun saveSettlement(
+        sourceId: Long,
+        destinationId: Long,
+        sourceType: SettlementType,
+        destinationType: SettlementType,
+        amount: BigDecimal,
+        ledAmount: BigDecimal,
+        currency: String,
+        ledCurrency: String,
+        settlementNum: String?,
+        signFlag: Short
+    ): Long? {
+        var settlementRecord = Settlement(
+            id = null,
+            amount = amount,
+            ledAmount = ledAmount,
+            createdBy = AresConstants.ARES_USER_ID,
+            createdAt = Timestamp.from(Instant.now()),
+            currency = currency,
+            ledCurrency = ledCurrency,
+            destinationId = destinationId,
+            destinationType = destinationType,
+            sourceId = sourceId,
+            sourceType = sourceType,
+            settlementDate = Date(),
+            settlementNum = settlementNum,
+            signFlag = signFlag,
+            settlementStatus = SettlementStatus.CREATED,
+            updatedAt = Timestamp.from(Instant.now()),
+            updatedBy = AresConstants.ARES_USER_ID
+        )
+
+        settlementRecord = settlementRepository.save(settlementRecord)
+
+        return settlementRecord.id
+    }
+
+    suspend fun savePayment(
+        accCode: Int,
+        accMode: AccMode,
+        amount: BigDecimal,
+        ledAmount: BigDecimal,
+        currency: String,
+        ledCurrency: String,
+        entityCode: Int,
+        exchangeRate: BigDecimal,
+        paymentNum: Long,
+        paymentNumValue: String,
+        signFlag: Short,
+        transRefNumber: String,
+        paymentCode: PaymentCode
+    ): Long? {
+        val payment = Payment(
+            id = null,
+            accCode = accCode,
+            accMode = accMode,
+            amount = amount,
+            ledAmount = ledAmount,
+            bankId = UUID.fromString("d646dc1c-f366-453c-b56f-2788f36c4136"),
+            organizationId = UUID.fromString("fb193031-64f6-417c-b538-9064d68db2bd"),
+            organizationName = "Inext Logistics & Supply Chain Private Limited",
+            tradePartyMappingId = UUID.fromString("7fc682ad-b547-4020-b81b-c95d5ee46d16"),
+            taggedOrganizationId = UUID.fromString("6b3bb3c6-4604-4485-acfc-d3fd2820383a"),
+            createdBy = UUID.fromString("ec306da2-0d52-4cc1-a7b1-d3a6541f1ce8"),
+            currency = currency,
+            ledCurrency = ledCurrency,
+            entityCode = entityCode,
+            exchangeRate = exchangeRate,
+            bankName = "rbl",
+            bankPayAmount = BigDecimal(0),
+            cogoAccountNo = "123456789876543",
+            migrated = false,
+            orgSerialId = 122334,
+            paymentCode = paymentCode,
+            paymentNumValue = paymentNumValue,
+            paymentNum = paymentNum,
+            paymentDocumentStatus = PaymentDocumentStatus.APPROVED,
+            payMode = PayMode.BANK,
+            refAccountNo = "123456789876543",
+            refPaymentId = "123456789876543",
+            sageOrganizationId = null,
+            sageRefNumber = null,
+            signFlag = signFlag,
+            transRefNumber = transRefNumber,
+            transactionDate = Date()
+        )
+
+        paymentRepo.save(payment)
+
+        saveAccountUtilizations(
+            accCode = payment.accCode,
+            accMode = payment.accMode,
+            documentStatus = DocumentStatus.FINAL,
+            documentNo = payment.paymentNum!!,
+            documentValue = payment.paymentNumValue!!,
+            accType = AccountType.valueOf(payment.paymentCode?.name!!),
+            amountCurr = payment.amount,
+            amountLoc = payment.ledAmount!!,
+            payLoc = BigDecimal(0),
+            payCurr = BigDecimal(0),
+            tdsAmountLoc = BigDecimal(0),
+            tdsAmount = BigDecimal(0),
+            signFlag = payment.signFlag,
+            currency = payment.currency,
+            ledCurrency = payment.ledCurrency!!,
+            entityCode = payment.entityCode
+        )
+
+        return payment.id
+    }
+
+    suspend fun saveParentJournalVoucher(
+        jvNum: String,
+        category: String,
+        currency: String,
+        ledCurrency: String,
+        description: String,
+        entityCode: Int,
+        exchangeRate: BigDecimal? = BigDecimal(1),
+        amount: BigDecimal?,
+        ledAmount: BigDecimal?
+    ): HashMap<String, Any> {
+        var parentJvData = ParentJournalVoucher(
+            id = null,
+            jvNum = jvNum,
+            status = JVStatus.APPROVED,
+            category = category,
+            createdBy = AresConstants.ARES_USER_ID,
+            currency = currency,
+            ledCurrency = ledCurrency,
+            description = description,
+            entityCode = entityCode,
+            exchangeRate = exchangeRate,
+            jvCodeNum = "AXIS",
+            transactionDate = Date(),
+            updatedAt = Timestamp.from(Instant.now()),
+            updatedBy = AresConstants.ARES_USER_ID,
+            validityDate = Date()
+        )
+
+        parentJvData = parentJvRepo.save(parentJvData)
+
+        val jvLineItems = saveJvLineItems(
+            parentJvData,
+            amount = amount,
+            ledAmount = ledAmount
+        )
+
+        return hashMapOf(
+            "parentJv" to parentJvData,
+            "jvLIneItems" to jvLineItems
+        )
+    }
+
+    private suspend fun saveJvLineItems(
+        parentJvData: ParentJournalVoucher,
+        amount: BigDecimal?,
+        ledAmount: BigDecimal?,
+    ): List<JournalVoucher> {
+        val jvLineItemsProps = getJvLineItemsProps()
+        val jvLineItemData = jvLineItemsProps.map { lineItem ->
+            JournalVoucher(
+                id = null,
+                jvNum = parentJvData.jvNum!!,
+                accMode = if (lineItem["accMode"] != null) AccMode.valueOf(lineItem["accMode"]!!.toString()) else AccMode.OTHER,
+                category = parentJvData.category,
+                createdAt = parentJvData.createdAt,
+                createdBy = parentJvData.createdBy,
+                updatedAt = parentJvData.createdAt,
+                updatedBy = parentJvData.createdBy,
+                currency = parentJvData.currency,
+                ledCurrency = parentJvData.ledCurrency!!,
+                amount = amount,
+                ledAmount = ledAmount,
+                description = parentJvData.description,
+                entityCode = parentJvData.entityCode,
+                entityId = UUID.fromString(AresConstants.ENTITY_ID[parentJvData.entityCode]),
+                exchangeRate = parentJvData.exchangeRate,
+                glCode = lineItem["glCode"].toString(),
+                parentJvId = parentJvData.id,
+                type = lineItem["type"].toString(),
+                signFlag = lineItem["signFlag"]?.toString()?.toShort(),
+                status = JVStatus.APPROVED,
+                tradePartyId = UUID.fromString("7fc682ad-b547-4020-b81b-c95d5ee46d16"),
+                tradePartyName = "VOLTAS LIMITED",
+                validityDate = parentJvData.transactionDate,
+                migrated = false,
+                deletedAt = null,
+                additionalDetails = JVAdditionalDetails(
+                    utr = parentJvData.description
+                )
+            )
+        }
+
+        val jvLineItems = journalVoucherRepository.saveAll(jvLineItemData)
+        jvLineItems.map {
+            if (it.accMode != AccMode.OTHER && it.accMode != null) {
+                saveAccountUtilizations(
+                    accMode = it.accMode!!,
+                    accCode = it.glCode?.toInt()!!,
+                    accType = AccountType.valueOf(parentJvData.category),
+                    amountCurr = it.amount!!,
+                    amountLoc = it.ledAmount!!,
+                    currency = it.currency!!,
+                    ledCurrency = it.ledCurrency,
+                    documentNo = it.id!!,
+                    documentValue = it.jvNum,
+                    documentStatus = DocumentStatus.FINAL,
+                    entityCode = it.entityCode!!,
+                    payCurr = BigDecimal(0),
+                    payLoc = BigDecimal(0),
+                    signFlag = it.signFlag!!,
+                    tdsAmount = BigDecimal(0),
+                    tdsAmountLoc = BigDecimal(0)
+                )
+            }
+        }
+
+        return jvLineItems
+    }
+
+    private fun getJvLineItemsProps(): MutableList<HashMap<String, Any?>> {
+        return mutableListOf(
+            hashMapOf(
+                "accMode" to "AP",
+                "glCode" to "321000",
+                "type" to "DEBIT",
+                "signFlag" to 1
+            ),
+            hashMapOf(
+                "accMode" to "VTDS",
+                "glCode" to "324003",
+                "type" to "CREDIT",
+                "signFlag" to -1
+            )
         )
     }
 }
--- api/src/test/kotlin/com/cogoport/SettlementTest.kt
+++ api/src/test/kotlin/com/cogoport/SettlementTest.kt
@@ -1,8 +1,12 @@
 package com.cogoport
 
-import com.cogoport.ares.api.common.AresConstants
 import com.cogoport.ares.api.payment.repository.AccountUtilizationRepo
 import com.cogoport.ares.api.utils.logger
+import com.cogoport.ares.model.payment.AccMode
+import com.cogoport.ares.model.payment.AccountType
+import com.cogoport.ares.model.payment.DocumentStatus
+import com.cogoport.ares.model.payment.PaymentCode
+import com.cogoport.ares.model.settlement.SettlementType
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
@@ -14,12 +18,12 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
-import com.cogoport.brahma.opensearch.Client as OpenSearchClient
+import java.math.BigDecimal
 
 @ExtendWith(MockitoExtension::class)
 @OptIn(ExperimentalCoroutinesApi::class)
 @MicronautTest(transactional = false)
-class SettlementTest () {
+class SettlementTest() {
     @Inject
     @field:Client("/payments")
     lateinit var client: HttpClient
@@ -30,23 +34,82 @@ class SettlementTest () {
     @Inject
     lateinit var helper: SettlementHelper
 
-
     @BeforeEach
     fun setUp() = runTest {
         accountUtilizationRepo.deleteAll()
-//        OpenSearchClient.deleteIndex(indexName = AresConstants.ACCOUNT_UTILIZATION_INDEX)
     }
 
     @AfterEach
     fun tearDown() = runTest {
         accountUtilizationRepo.deleteAll()
-//        OpenSearchClient.deleteIndex(indexName = AresConstants.ACCOUNT_UTILIZATION_INDEX)
     }
 
     @Test
     fun settle() = runTest {
-//        val endpoint = "/settlement/"
-        val id = helper.saveAccountUtilizations()
-        logger().info("saved dfocument id: $id")
+        val accUtilId = helper.saveAccountUtilizations(
+            AccMode.AP,
+            AccountType.PINV,
+            321000,
+            "VIVEK/12234",
+            12345,
+            -1,
+            DocumentStatus.FINAL,
+            301,
+            BigDecimal(40),
+            BigDecimal(40),
+            "INR",
+            "INR",
+            BigDecimal(20),
+            BigDecimal(20),
+            BigDecimal(100),
+            BigDecimal(100)
+        )
+        logger().info("saved document id: $accUtilId")
+
+        val settlementId = helper.saveSettlement(
+            amount = BigDecimal(100),
+            ledAmount = BigDecimal(100),
+            currency = "INR",
+            ledCurrency = "INR",
+            destinationType = SettlementType.SINV,
+            destinationId = 122334,
+            sourceType = SettlementType.REC,
+            sourceId = 1000000,
+            settlementNum = "SETL12345",
+            signFlag = 1
+        )
+
+        logger().info("settlement with id was saved :$settlementId")
+        val paymentId = helper.savePayment(
+            321000,
+            AccMode.AP,
+            amount = BigDecimal(100),
+            ledAmount = BigDecimal(100),
+            currency = "INR",
+            ledCurrency = "INR",
+            entityCode = 301,
+            exchangeRate = BigDecimal(1),
+            paymentCode = PaymentCode.PAY,
+            paymentNum = 5645328567,
+            paymentNumValue = "PAY564532845",
+            signFlag = 1,
+            transRefNumber = "Payment Against VIvek garg"
+        )
+
+        logger().info("payment with id was saved :$paymentId")
+
+        val jvData = helper.saveParentJournalVoucher(
+            "JV/2324/1233",
+            "MISC",
+            "INR",
+            "INR",
+            "PAYMENT AGAINST 12345678",
+            301,
+            BigDecimal(1),
+            BigDecimal(100),
+            BigDecimal(100),
+        )
+
+        logger().info("parent jv: $jvData")
     }
 }
