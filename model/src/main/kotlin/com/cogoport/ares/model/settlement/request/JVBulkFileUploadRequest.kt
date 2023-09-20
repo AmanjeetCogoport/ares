c1e7f51a21727b220c1330c2b9155a179dab799c
Md Zaid Alam <zaid.alam@cogoport.com>
2023-09-20 16:19:52 +0530

    Resolved minor bugs

--- model/src/main/kotlin/com/cogoport/ares/model/settlement/request/JVBulkFileUploadRequest.kt
+++ model/src/main/kotlin/com/cogoport/ares/model/settlement/request/JVBulkFileUploadRequest.kt
@@ -7,8 +7,8 @@ import javax.validation.constraints.NotNull
 @Introspected
 data class JVBulkFileUploadRequest(
     @field:NotNull(message = "url is required")
-    var url: String,
+    var documentUrl: String,
     @field:NotNull(message = "performedById is required")
-    var performedById: UUID,
+    var performedByUserId: UUID,
     var performedByUserType: String?
 )
